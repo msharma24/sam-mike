85b9fe555e275052cb19cd91fd153ab62a864b76
Mukesh Sharma <mukeshsharma24@gmail.com>
2022-03-30 17:01:18 +1300

    Add ec2

---
+++ ec2.tf
@@ -0,0 +1,32 @@
+module "vpc_ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.4.0"
+
+  name = "${var.environment}/vpc_instance"
+  # instance_count = 1
+
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t3.xlarge"
+  monitoring             = true
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+  iam_instance_profile   = module.spoke_instance_iam_assumable_role.iam_instance_profile_id
+  key_name               = "koptest"
+  user_data              = <<EOF
+  #!/bin/bash
+  echo "Hello"
+  EOF
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  name_regex  = "amzn2-ami-hvm*"
+}
